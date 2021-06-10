b7661dd47f550b8e0da8d930f7fa4ac4b7a771a2
Abel García de Prada <agdeprada@izertis.com>
2021-06-10 14:19:04 +0200

    Add a new composable to show the avatar and name of the author

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,11 +3,22 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeplayground.data.users
+import com.example.composeplayground.domain.User
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +28,7 @@ class MainActivity : ComponentActivity() {
             ComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    AuthorInfoSection(author = users[0])
                 }
             }
         }
@@ -25,14 +36,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AuthorInfoSection(
+    author: User
+) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        RowImageWithText(
+            imageId = author.avatarId,
+            text = author.name,
+            modifier = Modifier.align(CenterVertically)
+        )
+        IconButton(
+            modifier = Modifier.align(CenterVertically),
+            onClick = {/* TODO */ }
+        ) {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = null,
+            )
+        }
+    }
+}
+
+@Composable
+fun RowImageWithText(
+    imageId: Int,
+    text: String,
+    modifier: Modifier
+) {
+    Row(
+        modifier = modifier,
+    ) {
+        Image(
+            painter = painterResource(id = imageId),
+            modifier = modifier
+                .size(40.dp)
+                .clip(CircleShape),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body1,
+            modifier = modifier
+                .padding(8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun AuthorInfoSectionPreview() {
     ComposePlaygroundTheme {
-        Greeting("Android")
+        AuthorInfoSection(users.first())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AuthorInfoSectionDarkPreview() {
+    ComposePlaygroundTheme(darkTheme = true) {
+        AuthorInfoSection(users.first())
     }
 }
