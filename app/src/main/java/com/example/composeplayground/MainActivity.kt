872456e3f7efc1badd22c4e4d4731284db1c3860
Abel García de Prada <agdeprada@izertis.com>
2021-06-11 12:46:27 +0200

    Retrieve posts from viewmodel

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeplayground.data.initialPosts
 import com.example.composeplayground.presentation.timeline.TimelineScreen
+import com.example.composeplayground.presentation.timeline.TimelineViewModel
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,9 +15,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val viewModel: TimelineViewModel by viewModels()
+
         setContent {
             ComposePlaygroundTheme {
-                TimelineScreen(initialPosts)
+                TimelineScreen(viewModel)
             }
         }
     }
@@ -26,6 +29,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TimelineScreenPreview() {
     ComposePlaygroundTheme {
-        TimelineScreen(initialPosts)
+        TimelineScreen()
     }
 }
--- app/src/main/java/com/example/composeplayground/presentation/timeline/TimelineScreen.kt
+++ app/src/main/java/com/example/composeplayground/presentation/timeline/TimelineScreen.kt
@@ -3,14 +3,23 @@ package com.example.composeplayground.presentation.timeline
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import com.example.composeplayground.domain.Post
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.composeplayground.data.initialPosts
 import com.example.composeplayground.presentation.post.PostItem
 
 @Composable
 fun TimelineScreen(
-    listOfPosts: List<Post>
+    timelineViewModel: TimelineViewModel = viewModel()
 ) {
+    // We initialize the state with at least one initialPost to make it work, but in real apps, we
+    // should handle this situation in a better way.
+    val timelineScreenState: TimelineScreenState by timelineViewModel.timelineScreenState.observeAsState(
+        initial = TimelineScreenState(listOf(initialPosts.first()))
+    )
+
     Surface(color = MaterialTheme.colors.background) {
-        PostItem(post = listOfPosts[0])
+        PostItem(post = timelineScreenState.listOfPosts.first())
     }
 }
